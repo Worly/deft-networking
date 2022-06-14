36d164226ceedb6687b01106f5484413750c26cc
Valentino Vukelic <tino.vukelic@gmail.com>
2022-06-14 16:34:55 +0200

    Bug fixes and added new unit test

--- DeftUnitTests/AF_HealthCheckTests.cs
+++ DeftUnitTests/AF_HealthCheckTests.cs
@@ -16,6 +16,11 @@ namespace DeftUnitTests
     [TestClass]
     public class AF_HealthCheckTests
     {
+        class Empty
+        {
+
+        }
+
         [TestMethod]
         public async Task WhenQuiet_ShouldAutoSendHealthCheck()
         {
@@ -69,6 +74,36 @@ namespace DeftUnitTests
             await Task.Delay((int)DeftConfig.Health.MaxQuietTime.TotalMilliseconds + 510 + DeftConfig.Health.HealthCheckTimeoutMs + 510);
 
             server.IsConnected.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public async Task WhenNotQuiet_ShouldNotSendHealthChecks()
+        {
+            var port = 8003;
+            var connectionIdentifier = "Server";
+
+            var remoteEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+
+            var clientListener = new ClientListener(port);
+
+            var server = await DeftConnector.ConnectAsync<Server>(remoteEndPoint, connectionIdentifier);
+
+            DeftConfig.Health.MaxQuietTime = TimeSpan.FromSeconds(1);
+
+            server.SendMethod<Empty, Empty>("DontCare", new Empty());
+            await Task.Delay(500);
+
+            server.SendMethod<Empty, Empty>("DontCare", new Empty());
+            await Task.Delay(500);
+
+            server.SendMethod<Empty, Empty>("DontCare", new Empty());
+            await Task.Delay(500);
+
+            server.SendMethod<Empty, Empty>("DontCare", new Empty());
+            await Task.Delay(500);
+
+            server.IsConnected.Should().BeTrue();
+            server.Connection.GetHealthStatus().NumberOfHealthChecksSent.Should().Be(0);
         }
     }
 
